816ee794070a2f7ab53879ce756aa43dc95515a8
Parulbansal1204 <pbansal1_be17@thapar.edu>
2020-05-21 00:23:04 +0530

    Updates

---
+++ precache-manifest.aef4cba8f948fb94d1164c62b7db4537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010767021a99425f8ba2dc41353d145d",
+    "url": "/movieSearch/index.html"
+  },
+  {
+    "revision": "c116b0d36423b23cf75d",
+    "url": "/movieSearch/static/css/main.2fad291a.chunk.css"
+  },
+  {
+    "revision": "c56b9fa24777782e47b5",
+    "url": "/movieSearch/static/js/2.4d0194fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movieSearch/static/js/2.4d0194fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c116b0d36423b23cf75d",
+    "url": "/movieSearch/static/js/main.8519ab14.chunk.js"
+  },
+  {
+    "revision": "75d706453f2493fc6a36",
+    "url": "/movieSearch/static/js/runtime-main.0cc9209d.js"
+  }
+]);
